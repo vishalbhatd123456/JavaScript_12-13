c2937132bd751391e9d882229cb6ae9f2897ef1d
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-12-13 14:51:36 +0530

    Create Using _The_Console.js

---
+++ Using _The_Console.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+console.log(123);
+console.log(true);
+
+
+var greeting = 'Hello';
+console.log(greeting);
+
+console.log([1,2,3,4]); //array of object numbers
+
+
+console.log({a:1, b:2});
+
+console.table({a:1,b:2});
+
+console.error('This is some error');
+
+
+//clear
+
+console.warn('This is a warning:');
+
+console.time('Hello');
+
+console.timeEnd('Hello'); //the time from begibing to the end
