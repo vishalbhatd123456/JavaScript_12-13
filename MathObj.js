d36e0e2af2fe86f77547c6aa06876b319c14612e
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-12-18 22:47:53 +0530

    Create MathObj.js

---
+++ MathObj.js
@@ -0,0 +1,24 @@
+const num1 = 100;
+const num2 = 50;
+
+let val; //mutating
+
+//simple math with numbers
+
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1/num2;
+val = num1%num2;
+
+//math objects
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+
+console.log(val);
+
